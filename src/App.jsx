21ea4f06857a5202eeb921713fff9fe2041107bd
Programing Hero <sajalb1899@gmail.com>
2023-09-17 23:01:02 +0600

    nav bar some work complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Navbar from './component/Navbar/Navbar'
+// import DaisyNav from './component/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
+      {/* <DaisyNav></DaisyNav> */}
       <h1 className='text-4xl bg-red-300'>React + vite</h1>
       
     </>
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '*', name: 'Not found' },
+      ];
+      
+    return (
+        <nav>
+            <div onClick={()=> setOpen(!open)} className="text-4xl">
+            {
+                open?<AiOutlineClose></AiOutlineClose>:<AiOutlineMenu></AiOutlineMenu>
+            }
+            
+            </div>
+            <ul className="md:flex">
+                {
+                    routes.map(route=><Link  key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
